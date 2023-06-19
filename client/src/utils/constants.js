c02ef5a322d23bf7529444e7ec3d27ab12f41d59
Vlad-Makarenko <Makrenkovladok@gmail.com>
2023-06-19 13:33:46 +0300

    constants updated

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,5 @@
-export const API_URL = 'http://localhost:5000/api';
-export const SERVER_URL = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://eventify-uusl.onrender.com/api';
+// export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = 'https://eventify-uusl.onrender.com';
 export const PAGE_LIMIT = 10;
